a32be5a1a3f4e48c6320cd6d490ffc18a59d856c
Roger Reyes <roreyem@mcaddbxd5.local>
2022-01-31 21:47:55 -0300

    parametro stages fix

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -2,7 +2,7 @@ def call(stages){
     def stagesList = stages.split(";")
 
     stagesList.each{
-        println("Stages enviados ===> ${it}")
+        println(it)
     }
 
     env.TAREA = "Paso 1: Build && Test"
